a4cf9f31bbddd143e159d3e925fe88bee6e7be21
guaracyalima <guaracyaraujolima@gmail.com>
2017-04-17 00:01:26 -0300

    Fractal in Angular - Complementations from Presenters and Transformers

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,8 @@ config.build_path_css = config.build_path + '/css';
 config.build_vendor_path_css = config.build_path_css + '/vendor';
 config.vendor_path_css = [
     config.bower_path + '/bootstrap/dist/css/bootstrap.min.css',
-    config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css'
+    config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css',
+    config.bower_path + '/angular-ui-notification/dist/angular-ui-notification.min.css',
 ];
 
 config.build_path_js = config.build_path + '/js';
@@ -33,9 +34,14 @@ config.vendor_path_js = [
     config.bower_path + '/angular-cookies/angular-cookies.min.js',
     config.bower_path + '/query-string/query-string.js',
     config.bower_path + '/angular-oauth2/dist/angular-oauth2.min.js',
+    config.bower_path + '/angular-ui-notification/dist/angular-ui-notification.min.js',
 ];
 
 config.build_path_html = config.build_path + '/views';
+config.build_path_font = config.build_path + '/fonts';
+config.build_path_images = config.build_path + '/images';
+
+
 
 gulp.task('copy-styles', function () {
     gulp.src(config.assets_path + '/css/**/*.css')
@@ -65,16 +71,28 @@ gulp.task('copy-html', function () {
     gulp.src(config.assets_path + '/js/views/**/*.html')
         .pipe(gulp.dest(config.build_path_html))
         .pipe(liveReload());
-})
+});
+
+gulp.task('copy-font', function () {
+    gulp.src(config.assets_path + '/fonts/**/*')
+        .pipe(gulp.dest(config.build_path_font))
+        .pipe(liveReload());
+});
+
+gulp.task('copy-images', function () {
+    gulp.src(config.assets_path + '/images/**/*')
+        .pipe(gulp.dest(config.build_path_images))
+        .pipe(liveReload());
+});
 
 gulp.task('watch-dev',['clear-build-folder'], function () {
     liveReload.listen();
-    gulp.start('copy-styles', 'copy-scripts', 'copy-html');
+    gulp.start('copy-styles', 'copy-scripts', 'copy-html',  'copy-font', 'copy-images');
     gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts', 'copy-html']);
 });
 
 gulp.task('default',['clear-build-folder'], function () {
-    gulp.start('copy-html');
+    gulp.start('copy-html', 'copy-font', 'copy-images');
     elixir(function (mix) {
        mix.styles(config.vendor_path_css
            .concat([config.assets_path + '/css/**/*.css']),
--- resources/assets/js/services/client.js
+++ resources/assets/js/services/client.js
@@ -6,14 +6,25 @@ angular.module('app.services').
                 function ($resource, appConfig) {
                         return $resource(appConfig.baseUrl + '/client/:id',
                             {
-                            id:'@id'
+                                id:'@id'
                             },
                             {
-                              update:
-                              {
-                                method: 'PUT'
-                              }
-                            }
-                        );
-
+                                update:
+                                    {
+                                        method: 'PUT'
+                                    }
+                                    // ,
+                                    //     query:
+                                    //         {
+                                    //             method: 'GET',
+                                    //             isArray: true,
+                                    //             transformResponse: function (data, headers)
+                                    //             {
+                                    //                 var dataJson = JSON.parse(data);
+                                    //                 dataJson = dataJson.data;
+                                    //                 console.log(dataJson);
+                                    //                 return dataJson;
+                                    //             }
+                                    //         }
+                            });
 }]);
